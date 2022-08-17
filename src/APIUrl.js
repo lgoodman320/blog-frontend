4c717434bf24a919f2118f288ce25db5b33e7e0d
@iamlarrygoodman <103057796+lgoodman320@users.noreply.github.com>
2022-08-17 18:05:14 -0400

    Update APIUrl to work with localhost or heroku

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "https://lgoodman320-blog-backend.herokuapp.com/";
+let APIUrl = "http://localhost:3001";
+
+// handle if we're running on heroku
+if (window.location.host.indexOf('.herokuapp.com') !== -1) {
+    APIUrl = "https://lgoodman320-blog-backend.herokuapp.com/";
+}
 
 export default APIUrl;
